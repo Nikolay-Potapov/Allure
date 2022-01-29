80ffdc8114f8d4dff2859807c8e5aeca2d5720d5
Nikolay Potapov <potapov4444gmail.ru>
2022-01-29 19:44:07 +0700

    first commit

---
+++ src/test/java/ru/netology/test/CardDeliveryWithDataGeneratorTest.java
@@ -0,0 +1,71 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.data.DataGenerator;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+class CardDeliveryWithDataGeneratorTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+
+    @Test
+    void shouldBookingACard() {
+        $("[data-test-id='city']  input").setValue(DataGenerator.generateCity());
+        $("[data-test-id='date']  input").doubleClick().sendKeys(Keys.BACK_SPACE);
+
+        String planMeetingDate = DataGenerator.generateDate(4);
+
+        $("[data-test-id='date']  input").setValue(planMeetingDate);
+        $("[data-test-id='name']  input").setValue(DataGenerator.generateName());
+        $("[data-test-id='phone']  input").setValue(DataGenerator.generatePhone());
+        $("[data-test-id='agreement']").click();
+        $(".grid-col button").click();
+
+        $(withText("Успешно!")).shouldBe(visible);
+        $(".notification__content").shouldBe(visible)
+                .shouldHave(exactText("Встреча успешно запланирована на " + planMeetingDate));
+
+        $("[data-test-id='date']  input").doubleClick().sendKeys(Keys.BACK_SPACE);
+
+        String replanMeetingDate = DataGenerator.generateDate(5);
+
+        $("[data-test-id='date']  input").setValue(replanMeetingDate);
+        $(".grid-col button").click();
+
+        $("[data-test-id='replan-notification'] .notification__content")
+                .shouldBe(visible).shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+
+        $(".notification_status_error .button").click();
+
+        $(withText("Успешно!")).shouldBe(visible);
+        $("[data-test-id='success-notification'] .notification__content").shouldBe(visible).shouldHave
+                (exactText("Встреча успешно запланирована на " + replanMeetingDate));
+
+    }
+}
